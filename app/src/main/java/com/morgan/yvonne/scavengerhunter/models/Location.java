2f40380e2fe9ec0173b850076f376463e0e4a670
YHop <ypeng.tw@gmail.com>
2015-11-13 10:10:50 -0800

    Modify Location models

--- app/src/main/java/com/morgan/yvonne/scavengerhunter/models/Location.java
+++ app/src/main/java/com/morgan/yvonne/scavengerhunter/models/Location.java
@@ -2,26 +2,15 @@ package com.morgan.yvonne.scavengerhunter.models;
 
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
 import android.widget.Toast;
 
-import com.google.android.gms.vision.barcode.Barcode;
-import com.morgan.yvonne.scavengerhunter.ui.LocationListActivity;
 import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
 
-import java.io.IOException;
-import java.util.List;
-
 public class Location extends Activity {
     ParseObject mLocationObject;
-
-    Context mContext;
 
     public Location(String address, String description, ParseGeoPoint geoPoint){
         mLocationObject = new ParseObject("Location");
@@ -31,7 +20,6 @@ public class Location extends Activity {
 
     }
 
-
     public String getAddressObject() {
         return mLocationObject.getString("address");
     }
@@ -40,15 +28,19 @@ public class Location extends Activity {
         return mLocationObject.getString("description");
     }
 
+    public ParseGeoPoint getGeoPoint(){
+        return mLocationObject.getParseGeoPoint("geoPoint");
+    }
+
     public void save(final Activity context, final Runnable runnable){
         mLocationObject.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
                 if(e == null){
-                    Toast.makeText(mContext.getApplicationContext(), "Save location succeffully", Toast.LENGTH_LONG).show();
+                    Toast.makeText(context.getApplicationContext(), "Save location succeffully", Toast.LENGTH_LONG).show();
                     context.runOnUiThread(runnable);
                 }else{
-                    Toast.makeText(mContext.getApplicationContext(), "Save location fail. Please try again", Toast.LENGTH_LONG).show();
+                    Toast.makeText(context.getApplicationContext(), "Save location fail. Please try again", Toast.LENGTH_LONG).show();
                 }
             }
         });
